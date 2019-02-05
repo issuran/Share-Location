837fab0cf0fe8fc95ea9065421a6e27732bf5f3e
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2019-02-05 19:25:35 -0200

    First Commit

---
+++ Share Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Share Location
+//
+//  Created by Tiago Oliveira on 05/02/19.
+//  Copyright © 2019 Tiago Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
