f9beb05e3570cc102d6473fa725daa52b7c2ef9a
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2019-04-14 12:05:51 -0300

    Adding Information post view controller

---
+++ Share Location/InformationPosting/Views/InformationPostViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InformationPostViewController.swift
+//  Share Location
+//
+//  Created by Tiago Oliveira on 14/04/19.
+//  Copyright © 2019 Tiago Oliveira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InformationPostViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancelAddAction(_ sender: Any) {
+        print("Cancel")
+    }
+    
+}
